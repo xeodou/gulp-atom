757d45b63688bcac74bc30af612372e0d65790da
xeodou <xeodou@gmail.com>
2014-05-15 22:46:59 +0800

    Init commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+# CPT
+
+> A template for npm module use coffee script
+
+## Getting Started
+
+### First
+Install `gulp`.
+If you haven't use `Gulp` , I think you should use it to save your short life.
+To install `gulp` you can have a look at this repo https://github.com/gulpjs/gulp or just use this command:
+
+```shell
+    npm i -g gulp
+```
+
+### Second
+Clone this repo:
+
+```shell
+    git clone git@github.com:xeodou/CPT.git
+```
+
+## Usage
+
+### Compile Coffee Script
+
+Use `gulp coffee` transfer coffeescript to javascript.
+
+### Test
+
+Use `gulp mocha` to test your code with mocha.
+
+
+
+
+## License
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+process.env.NODE_ENV = 'test';
+require('should');
+
+var path = require('path');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
+var mocha = require('gulp-mocha');
+
+// Files.
+var src = 'src/**/*.coffee';
+var tests = 'test/*.mocha.js';
+
+// Coffee Lint
+gulp.task('lint', function() {
+    gulp.src(src)
+        .pipe(coffeelint({
+            max_line_length: {
+                value: 100
+            },
+            indentation: {
+                value: 4
+            }
+        }))
+        .pipe(coffeelint.reporter());
+});
+
+// Compile coffee scripts.
+gulp.task('coffee', ['lint'], function() {
+    return gulp.src(src)
+        .pipe(coffee({
+            bare: true
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('lib'))
+        .on('error', gutil.log);
+});
+
+// Run tests.
+gulp.task('mocha', ['coffee'], function() {
+    return gulp.src(tests)
+        .pipe(mocha({
+            timeout: 10000,
+            reporter: 'spec'
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(src, ['coffee']);
+});
+
+gulp.task('default', ['coffee']);
